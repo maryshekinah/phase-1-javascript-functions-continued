c44df269a7acb9fc46ba42f36c833c9fe04cefb5
shekinah <nyamburamary1212@gmail.com>
2023-09-19 14:44:37 +0300

    Completed continued-functions

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+    function saturdayFun(program = 'roller-skate'){
+    return (`This Saturday, I want to ${program}!`);
+}
+function mondayWork(program= 'go to the office'){
+    return `This Monday, I will ${program}.`;
+}
+function wrapAdjective(action= "*"){
+    return function(adjective = 'slow'){
+        return `You are ${action}${adjective}${action}!`;
+    }
+}
